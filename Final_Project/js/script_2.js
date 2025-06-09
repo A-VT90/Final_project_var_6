b484293998222b46fb0a9e7645a154acc5416553
A-VT90 <atupyakov@yandex.ru>
2025-06-09 13:36:28 +0300

    Delete Final_Project directory

--- Final_Project/js/script_2.js
+++
@@ -1,6 +0,0 @@
-// Обработка формы контактов
-document.querySelector('.contact-form')?.addEventListener('submit', e => {
-  e.preventDefault();
-  alert('Сообщение отправлено!');
-  e.target.reset();
-}); 
